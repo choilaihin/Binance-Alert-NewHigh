4adac5b31405e92e0831cb127fa31a71b1060224
Kent <lai-hin@hotmail.com>
2021-09-29 15:49:04 +0800

    updates

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This script notifies the user via Telegram whenever an USDT cryptocurrency pair 
 2. Filter away unwanted tickers and store remaining ticker symbols into a list
 3. Get historical daily candlesticks for these tickers and store into a SQLite database table for each ticker
 4. Store the 30 day, 60 day, all time high value into a dictionary
-5. Continuously compare current price with the dictionary values and send a Telegram message if greater than
+5. Continuously compare current price with the dictionary values and send a Telegram message if exceeded
 6. Update the list of tickers and database at the start of the next day (UTC +0)
 
 ## Demo
@@ -21,8 +21,23 @@ This shows the Telegram messages received when an alert is triggered:
 
 ## Technologies Used
 
-- Using python-binance ThreadedWebsocketManager Websocket  
+- Using python-binance websockets  
   https://python-binance.readthedocs.io/en/latest/index.html
 
 - Parsing candlestick data  
   https://github.com/binance-us/binance-official-api-docs/blob/master/rest-api.md#klinecandlestick-data
+
+- SQLite database for storing candlestick data  
+  https://docs.python.org/3/library/sqlite3.html
+
+- Sending Telegram messages  
+  https://core.telegram.org/bots#6-botfather
+
+## Setup
+
+1. git clone https://github.com/choilaihin/Binance-Alert-NewHigh.git
+2. pip install -r requirements.txt
+3. Setup a Binance account and place your Binance API Key and API Secret into a .env file. Refer to https://www.binance.com/en/support/faq/360002502072 for instructions.
+4. Setup a Telegram bot and place your bot token and chatID into the .env file. Refer to https://core.telegram.org/bots#6-botfather for instructions.
+5. Upon executing python main.py, a logging file 'dailyhigh.log' and database file 'daily_database.db' is created. For the first run, it will take around 10 mins to populate the database with historical candlestick data since 1st Jan 2021.
+6. Once the database is populated, the variable first_run can be set to False for subsequent executions of main.py.
--- main.py
+++ main.py
@@ -10,36 +10,46 @@ Description:
 5. Continuously compare current price with the dictionary value and send a Telegram message if greater
 6. Update the list of tickers and database at the start of the next day (UTC +0)
 '''
-
-import config, sqlite3, datetime, time, requests, logging
+from decouple import config
+import sqlite3
+import datetime
+import time
+import requests
+import logging
 # python-binance library
 from binance import ThreadedWebsocketManager
 from binance.client import Client
 
 # Setting up Binance API client
-api_key = config.API_KEY
-api_secret = config.API_SECRET
+api_key = config('API_KEY')
+api_secret = config('API_SECRET')
 client = Client(api_key, api_secret)
 
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
+
 def setup_logger(name, log_file, level):
-    handler = logging.FileHandler(log_file)        
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(handler)
     return logger
 
+
 loggerdailyhigh = setup_logger('first_logger', 'dailyhigh.log', logging.INFO)
 
 # function to send telegram message
+
+
 def telegram_bot_sendtext(bot_message):
-    bot_token = config.bot_token
-    bot_chatID = config.bot_chatID
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    bot_token = config('bot_token')
+    bot_chatID = config('bot_chatID')
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
+
 
 record = True
 record_time = datetime.datetime.now()
@@ -47,11 +57,12 @@ initialisation_1min = record_time + datetime.timedelta(seconds=20)
 initialisation_phase = True
 database_phase = False
 # Set first_run to True if database is not available
-first_run = False
+first_run = True
 ticker_list = []
 ticker_high_dict = {}
 streams = ['!ticker@arr']
 loggerdailyhigh.info("Getting desired symbols...")
+
 
 def main():
     # Initialise ThreadedWebsocketManager
@@ -60,8 +71,9 @@ def main():
     twm.start()
 
     # Exclude undesired symbols such as stable coins
-    exclusion_usdt_symbols = ["BUSDUSDT", "USDCUSDT", "TUSDUSDT", "SUSDUSDT","PAXUSDT"]
-    
+    exclusion_usdt_symbols = ["BUSDUSDT", "USDCUSDT",
+                              "TUSDUSDT", "SUSDUSDT", "PAXUSDT", "USDPUSDT"]
+
     # This function is executed whenever streaming data is received.
     def handle_socket_message(msg):
         global record, record_time, initialisation_phase, database_phase, initialisation_1min, first_run, ticker_list, ticker_high_dict, streams
@@ -76,11 +88,12 @@ def main():
             initialisation_1min = record_time + datetime.timedelta(seconds=20)
 
         # variables for transitioning to database updating phase which runs once a day too
-        if now>initialisation_1min and initialisation_phase:
+        if now > initialisation_1min and initialisation_phase:
             initialisation_phase = False
             record = False
             database_phase = True
-            loggerdailyhigh.info(f"There are {len(ticker_list)} symbols, updating database...")
+            loggerdailyhigh.info(
+                f"There are {len(ticker_list)} symbols, updating database...")
 
         # record symbol and price of desired tickers into a list
         if msg['stream'] == '!ticker@arr' and record:
@@ -96,17 +109,19 @@ def main():
             try:
                 conn = sqlite3.connect('daily_database.db')
             except Exception as e:
-                loggerdailyhigh.info(f"Unable to connect to database. Error: {e}")
+                loggerdailyhigh.info(
+                    f"Unable to connect to database. Error: {e}")
 
             for symbol in ticker_list:
                 # SQLite does not allow table name to start with a number (e.g. 1INCHUSDT) thus "1INCHUSDT" is used
                 symbol_string = '"' + symbol + '"'
                 try:
                     # Check whether table exist for symbol
-                    cursor = conn.execute(f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name={symbol_string}")
+                    cursor = conn.execute(
+                        f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name={symbol_string}")
 
                     # create table for symbol if does not exist
-                    if cursor.fetchone()[0]!=1:
+                    if cursor.fetchone()[0] != 1:
                         conn.execute(f'''CREATE TABLE {symbol_string}
                             (TIME REAL PRIMARY KEY     NOT NULL,
                             OPEN           REAL    NOT NULL,
@@ -114,31 +129,38 @@ def main():
                             LOW           REAL    NOT NULL,
                             CLOSE           REAL    NOT NULL,
                             VOLUME           REAL    NOT NULL);''')
-                    
+
                     # get historical data, only update previous day's data if database already exist
                     if first_run:
-                        candlesticks= client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1DAY, "1 Jan, 2021", "10 Sep, 2021")
+                        candlesticks = client.get_historical_klines(
+                            symbol, Client.KLINE_INTERVAL_1DAY, "1 Jan, 2021", "10 Sep, 2021")
                     else:
-                        candlesticks= client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1DAY, "2 day ago UTC")
+                        candlesticks = client.get_historical_klines(
+                            symbol, Client.KLINE_INTERVAL_1DAY, "2 day ago UTC")
 
                     # Update database
                     start_inserting = False
                     for candlestick in candlesticks:
                         if start_inserting:
                             # insert the remaining candlesticks without checking for existence
-                            conn.execute(f"INSERT INTO {symbol_string} (TIME,OPEN,HIGH,LOW,CLOSE,VOLUME) VALUES ({candlestick[0]},{float(candlestick[1])},{float(candlestick[2])},{float(candlestick[3])},{float(candlestick[4])},{float(candlestick[5])})")
+                            conn.execute(
+                                f"INSERT INTO {symbol_string} (TIME,OPEN,HIGH,LOW,CLOSE,VOLUME) VALUES ({candlestick[0]},{float(candlestick[1])},{float(candlestick[2])},{float(candlestick[3])},{float(candlestick[4])},{float(candlestick[5])})")
                         else:
                             # check whether next candlestick exist in table
-                            cursor = conn.execute(f"SELECT rowid FROM {symbol_string} WHERE TIME = ?", (candlestick[0]+86400000,))
+                            cursor = conn.execute(
+                                f"SELECT rowid FROM {symbol_string} WHERE TIME = ?", (candlestick[0]+86400000,))
                             if cursor.fetchone() is None:
                                 # insert/update current candlestick and insert rest if the next candlestick is not found
-                                cursor = conn.execute(f"SELECT rowid FROM {symbol_string} WHERE TIME = ?", (candlestick[0],))
+                                cursor = conn.execute(
+                                    f"SELECT rowid FROM {symbol_string} WHERE TIME = ?", (candlestick[0],))
                                 if cursor.fetchone() is None:
                                     # insert current candlestick since it is not found too
-                                    conn.execute(f"INSERT INTO {symbol_string} (TIME,OPEN,HIGH,LOW,CLOSE,VOLUME) VALUES ({candlestick[0]},{float(candlestick[1])},{float(candlestick[2])},{float(candlestick[3])},{float(candlestick[4])},{float(candlestick[5])})")
+                                    conn.execute(
+                                        f"INSERT INTO {symbol_string} (TIME,OPEN,HIGH,LOW,CLOSE,VOLUME) VALUES ({candlestick[0]},{float(candlestick[1])},{float(candlestick[2])},{float(candlestick[3])},{float(candlestick[4])},{float(candlestick[5])})")
                                 else:
                                     # update current candlestick since it exists
-                                    conn.execute(f"UPDATE {symbol_string} set OPEN = ?, HIGH = ?, LOW = ?, CLOSE = ?, VOLUME = ? where TIME = {candlestick[0]}", (float(candlestick[1]),float(candlestick[2]),float(candlestick[3]),float(candlestick[4]),float(candlestick[5]),))
+                                    conn.execute(f"UPDATE {symbol_string} set OPEN = ?, HIGH = ?, LOW = ?, CLOSE = ?, VOLUME = ? where TIME = {candlestick[0]}", (float(
+                                        candlestick[1]), float(candlestick[2]), float(candlestick[3]), float(candlestick[4]), float(candlestick[5]),))
                                 # insert the remaining candlesticks without checking for existence
                                 start_inserting = True
                 except Exception as e:
@@ -155,11 +177,15 @@ def main():
                 symbol_string = '"' + symbol + '"'
                 try:
                     # obtain the maximum high of the respective intervals (e.g. 30 day, 60 day or all time high)
-                    cursor_30d = conn.execute(f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME > {lower_thres_time_30d} AND TIME < {upper_thres_time}")
-                    cursor_60d = conn.execute(f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME > {lower_thres_time_60d} AND TIME < {upper_thres_time}")
-                    cursor_alltime = conn.execute(f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME < {upper_thres_time}")
+                    cursor_30d = conn.execute(
+                        f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME > {lower_thres_time_30d} AND TIME < {upper_thres_time}")
+                    cursor_60d = conn.execute(
+                        f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME > {lower_thres_time_60d} AND TIME < {upper_thres_time}")
+                    cursor_alltime = conn.execute(
+                        f"SELECT MAX(HIGH) FROM {symbol_string} WHERE TIME < {upper_thres_time}")
                     # store the values into a dictionary
-                    ticker_high_dict[symbol]= {'alltime':cursor_alltime.fetchone()[0],'60d':cursor_60d.fetchone()[0],'30d':cursor_30d.fetchone()[0]}
+                    ticker_high_dict[symbol] = {'alltime': cursor_alltime.fetchone(
+                    )[0], '60d': cursor_60d.fetchone()[0], '30d': cursor_30d.fetchone()[0]}
                 except Exception as e:
                     loggerdailyhigh.info(f"Error for {symbol}: {e}")
             try:
@@ -179,31 +205,38 @@ def main():
                     if symbol in ticker_high_dict and price and ticker_high_dict[symbol]['alltime'] and ticker_high_dict[symbol]['60d'] and ticker_high_dict[symbol]['30d']:
                         if price > ticker_high_dict[symbol]['alltime']:
                             now = datetime.datetime.now()
-                            telegram_bot_sendtext(f"{now:%Y-%m-%d %H:%M:%S} {symbol}\nall time high of {price}")
-                            loggerdailyhigh.info(f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached all time high of {price}')
+                            telegram_bot_sendtext(
+                                f"{now:%Y-%m-%d %H:%M:%S} {symbol}\nall time high of {price}")
+                            loggerdailyhigh.info(
+                                f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached all time high of {price}')
                             # to prevent repeat alert for the day
                             del ticker_high_dict[symbol]
                             # to skip subsequent comparisons for this symbol
                             continue
                         if price > ticker_high_dict[symbol]['60d']:
                             now = datetime.datetime.now()
-                            telegram_bot_sendtext(f"{now:%Y-%m-%d %H:%M:%S} {symbol}\n60 day high of {price}")
-                            loggerdailyhigh.info(f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached 60 day high of {price}')
+                            telegram_bot_sendtext(
+                                f"{now:%Y-%m-%d %H:%M:%S} {symbol}\n60 day high of {price}")
+                            loggerdailyhigh.info(
+                                f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached 60 day high of {price}')
                             del ticker_high_dict[symbol]
                             continue
                         if price > ticker_high_dict[symbol]['30d']:
                             now = datetime.datetime.now()
-                            telegram_bot_sendtext(f"{now:%Y-%m-%d %H:%M:%S} {symbol}\n30 day high of {price}")
-                            loggerdailyhigh.info(f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached 30 day high of {price}')
+                            telegram_bot_sendtext(
+                                f"{now:%Y-%m-%d %H:%M:%S} {symbol}\n30 day high of {price}")
+                            loggerdailyhigh.info(
+                                f'{now:%Y-%m-%d %H:%M:%S} {symbol} reached 30 day high of {price}')
                             del ticker_high_dict[symbol]
-                        
- 
-    stream_name = twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
+
+    stream_name = twm.start_multiplex_socket(
+        callback=handle_socket_message, streams=streams)
     # Join to main thread to keep the ThreadedWebsocketManager running.
     twm.join()
 
+
 if __name__ == "__main__":
-   main()
+    main()
 
 
 '''
